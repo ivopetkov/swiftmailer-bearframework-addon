066097eca9308d35df63c6b1d9469c95bc2ba22e
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 14:17:45 +0300

    Updated comments.

--- classes/SwiftMailer.php
+++ classes/SwiftMailer.php
@@ -3,7 +3,7 @@
 /*
  * Swift Mailer addon for Bear Framework
  * https://github.com/ivopetkov/swiftmailer-bearframework-addon
- * Copyright (c) 2017 Ivo Petkov
+ * Copyright (c) Ivo Petkov
  * Free to use under the MIT license.
  */
 
--- ide-helper.php
+++ ide-helper.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Swift Mailer addon for Bear Framework
+ * https://github.com/ivopetkov/swiftmailer-bearframework-addon
+ * Copyright (c) Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
 namespace BearFramework {
 
     /**
