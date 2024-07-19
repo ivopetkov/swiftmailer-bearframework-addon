cc906d9709648e7c97b8adecec6cffd2dfbe4346
Ivo Petkov <ivo@ivopetkov.com>
2024-07-19 20:12:02 +0300

    Update unit tests.

--- tests/SwiftMailerTest.php
+++ tests/SwiftMailerTest.php
@@ -73,8 +73,11 @@ class SwiftMailerTest extends BearFramework\AddonTests\PHPUnitTestCase
         $raw = $message->toString();
         $raw = preg_replace('/Message\-ID\: \<(.*)\.john\@example\.com\>/', 'Message-ID: <xxx111.john@example.com>', $raw);
         $raw = preg_replace('/Date\: (.*)/', 'Date: xxx222', $raw);
-        $raw = preg_replace('/boundary\-([a-z0-9=]*)/', 'boundary-xxx333', $raw);
-        $raw = preg_replace('/\_\=\_swift\_(.*?)\_\=\_/', 'boundary-xxx444', $raw);
+        $matches = null;
+        preg_match_all('/\_\=\_(.*?)\_\=\_/', $raw, $matches);
+        $boundaries = array_values(array_unique($matches[0]));
+        $raw = join('xxx333', explode($boundaries[0], $raw));
+        $raw = join('xxx444', explode($boundaries[1], $raw));
         $raw = trim($raw);
         $raw = preg_replace('~\r\n?~', "\n", $raw);
 
@@ -88,48 +91,48 @@ To: Mark <mark@example.com>
 Cc: Mark <tom@example.com>
 Bcc: Mark <sam@example.com>
 MIME-Version: 1.0
-Content-Type: multipart/mixed;
- boundary=boundary-xxx333
+Content-Type: multipart/related;
+ boundary="xxx333"
 X-Priority: 1 (Highest)
 X-Custom-2: value2
 X-Custom-1: value1
 
 
---boundary-xxx333
+--xxx333
 Content-Type: multipart/alternative;
- boundary="boundary-xxx444"
+ boundary="xxx444"
 
 
---boundary-xxx444
+--xxx444
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: quoted-printable
 
 Hi there
 
---boundary-xxx444
+--xxx444
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: quoted-printable
 
 <strong>Hi</strong>
 
---boundary-xxx444--
+--xxx444--
 
 
---boundary-xxx333
+--xxx333
 Content-Type: image/png; name=file1.png
 Content-Transfer-Encoding: base64
 Content-Disposition: attachment; filename=file1.png
 
 Y29udGVudDE=
 
---boundary-xxx333
+--xxx333
 Content-Type: image/png; name=file2.png
 Content-Transfer-Encoding: base64
 Content-Disposition: attachment; filename=file2.png
 
 Y29udGVudDI=
 
---boundary-xxx333
+--xxx333
 Content-Type: image/jpg; name=fileA.jpg
 Content-Transfer-Encoding: base64
 Content-ID: <fileA@exaple.com>
@@ -137,7 +140,7 @@ Content-Disposition: inline; filename=fileA.jpg
 
 Y29udGVudEE=
 
---boundary-xxx333
+--xxx333
 Content-Type: image/jpg; name=fileB.jpg
 Content-Transfer-Encoding: base64
 Content-ID: <fileB@exaple.com>
@@ -145,7 +148,7 @@ Content-Disposition: inline; filename=fileB.jpg
 
 Y29udGVudEI=
 
---boundary-xxx333--';
+--xxx333--';
         $expected = preg_replace('~\r\n?~', "\n", $expected);
         $this->assertTrue($raw === $expected);
     }
